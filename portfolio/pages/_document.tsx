32c0bf77608357bec441ff3fcd5a45056a63cf55
Siying Chen <chensiyingcrystal@gmail.com>
2023-07-06 17:42:45 -0400

    update to-do

--- portfolio/pages/_document.tsx
+++ portfolio/pages/_document.tsx
@@ -1,9 +1,12 @@
+/* eslint-disable react/no-unescaped-entities */
+
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
+      <title>Siying's portfolio</title>
       <body>
         <Main />
         <NextScript />
